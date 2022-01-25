100777569278f0097b1135b3eb79d4238afa2a31
EC2 Default User <ec2-user@ip-172-31-17-53.ec2.internal>
2022-01-25 02:16:15 +0000

    [update]メッセージエラー改善

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,7 +14,8 @@ class BooksController < ApplicationController
       flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
-      render :index
+      @books = Book.all
+       render :index
     end
   end
 
